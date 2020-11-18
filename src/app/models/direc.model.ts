5b0954323521d6a84556bb7f6c0618eb53ac3f7b
Majo Burguete <marijo.mjbe@gmail.com>
2020-11-17 18:48:30 -0600

    Nueva direccion

---
+++ src/app/models/direc.model.ts
@@ -0,0 +1,9 @@
+export interface DirecModel{
+    street: string;
+    city: string;
+    state: string;
+    zip_code: string;
+    country: string;
+    phone_number: number;
+    instructions: string;
+}
